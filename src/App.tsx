c92a52ebd54ec7c91bcd5e1393abbc97f913c652
Umutcan <umutcankus@umutcans-mbp.mynet>
2021-05-25 23:55:59 +0300

    Change routing method

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import { WordCard } from './components/word-card/WordCard';
-import { BrowserRouter as HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter basename='/sozluk'>
+      <Router basename={process.env.PUBLIC_URL}>
         <Route path="/:word/" exact component={WordCard} />
         <Route path="/" exact component={App} />
-      </HashRouter>
+      </Router>
     </div>
   );
 }
